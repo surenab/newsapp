6744fbf35377dbdcfb00d80a22a53b2da73d7261
avagyanie <avagyannie@gmail.com>
2023-07-31 17:34:49 +0400

    registration done

--- newsapp/login/forms.py
+++ newsapp/login/forms.py
@@ -7,4 +7,4 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "first_name", "last_name", "password1", "password2", "email"]
+        fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
