3ff1d11cc99bb64dbba4e9002904bb4d23444284
nairaasa <asatryan.nairah@gmail.com>
2023-08-01 21:23:37 +0400

    models created

--- newsapp/core/models.py
+++ newsapp/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+
+    NEWS_TYPES = (
+        ("1", "Urgent"),
+        ("2", "Regular"),
+        ("3", "Low")
+    )
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    publication_date = models.DateTimeField(auto_now_add=True)
+    newsapp_type = models.CharField(choices=NEWS_TYPES, default="2", max_length=1)
+
+    def __str__(self):
+        return f"{self.title}"
