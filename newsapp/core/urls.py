a00b234b22bf8160d1d31f2f8e7ebeddccb63de2
Narine <naradamyan995@gmail.com>
2023-08-07 14:22:48 +0400

    about.html

--- newsapp/core/urls.py
+++ newsapp/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, CreateNews
+from .views import home, CreateNews, about
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,4 +7,5 @@ from django.conf import settings
 urlpatterns = [
     path('', home, name = "home"),
     path('create-news', CreateNews.as_view(), name = "create_news"),
+    path('about', about, name = "about"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
