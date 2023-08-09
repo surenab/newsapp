f3a72a5aec369e0f2532674dd3741051a36580c3
avagyanie <avagyannie@gmail.com>
2023-08-09 23:16:57 +0400

    MyNews added

--- newsapp/core/views.py
+++ newsapp/core/views.py
@@ -1,8 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from .models import *
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .forms import NewsForm
 from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -18,6 +22,7 @@ class CreateNews(CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        messages.success(self.request, "Your new post has been completed.")
         return super().form_valid(form)
 
 
@@ -26,3 +31,52 @@ def about(request):
     return render(request=request, template_name="about.html", context={"team": team}) 
 
 
+class MyNews(ListView):
+    model = News
+    context_object_name = "news"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super(MyNews, self).get_queryset()
+        queryset = queryset.filter(user=self.request.user)
+        return queryset
+
+
+# class MyNewsDetail(LoginRequiredMixin, DetailView):
+#     model = News
+#     context_object_name = "news"
+
+#     def get_queryset(self):
+#         queryset = super(MyNewsDetail, self).get_queryset()
+#         queryset = queryset.filter(user=self.request.user)
+#         return queryset
+
+
+# class MyNewsUpdate(LoginRequiredMixin, UpdateView):
+#     model = News
+#     context_object_name = "news"
+#     form_class = NewsForm
+#     success_url = reverse_lazy("my_news")
+
+#     def get_queryset(self):
+#         queryset = super(MyNewsUpdate, self).get_queryset()
+#         queryset = queryset.filter(user=self.request.user)
+#         return queryset
+
+#     def form_valid(self, form):
+#         messages.success(self.request, "News instance is updated!")
+#         return super().form_valid(form)
+
+
+# class MyNewsDelete(LoginRequiredMixin, DeleteView):
+#     model = News
+#     context_object_name = "news"
+#     success_url = reverse_lazy("my_news")
+
+#     def get_queryset(self):
+#         queryset = super(MyNewsDelete, self).get_queryset()
+#         queryset = queryset.filter(user=self.request.user)
+#         return queryset
+
+#     def form_valid(self, form):
+#         messages.info(self.request, "News instance is deleted!")
+#         return super().form_valid(form)
