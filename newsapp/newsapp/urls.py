03871592414f49303ed9a5547f7c211ac4025ba9
avagyanie <avagyannie@gmail.com>
2023-08-21 20:13:30 +0400

    urlpatterns +=

--- newsapp/newsapp/urls.py
+++ newsapp/newsapp/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('accounts/', include("login.urls")),
     path('accounts/', include("login.urls")),
     path('', include("core.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
