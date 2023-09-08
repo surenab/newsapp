906ec2c12a839db5cf5857ce35ef9065f726aae4
nairaasa <asatryan.nairah@gmail.com>
2023-09-08 11:32:54 +0400

    created message for successfully registration

--- newsapp/login/urls.py
+++ newsapp/login/urls.py
@@ -2,9 +2,10 @@ from django.urls import path, include
 from .views import Register
 from django.conf.urls.static import static
 from django.conf import settings
-
+from . import views
 
 urlpatterns = [
     path('', include("django.contrib.auth.urls")),
     path('register/', Register.as_view(), name = "register"),
+    path('registration-success/', views.registration_success, name='registration-success'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- newsapp/login/views.py
+++ newsapp/login/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import RegisterForm
+from django.contrib import messages
 
 
 # Create your views here.
 
 class Register(CreateView):
     form_class = RegisterForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("registration-success")
     template_name = "registration/register.html"
 
+def register(request):
+    if request.method == 'POST':
+        
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            messages.success(request, 'Registration successful!')
+            return redirect('registration-success')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/register.html', {'form': form})
+
+def registration_success(request):
+    return render(request, 'registration/registration_success.html')
