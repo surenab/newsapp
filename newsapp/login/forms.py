85d46a1c025052550fb7adfb078bc4260582f2d9
avagyanie <avagyannie@gmail.com>
2023-09-11 12:07:08 +0400

    profile - DONE

--- newsapp/login/forms.py
+++ newsapp/login/forms.py
@@ -1,10 +1,77 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+from django.contrib.auth.forms import SetPasswordForm
+from django.contrib.auth import get_user_model
 
 
 class RegisterForm(UserCreationForm):
+    username = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    email = forms.EmailField(label='', widget=forms.TextInput(attrs={'placeholder': 'Email address'}))
+    password1 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+    first_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
 
     class Meta:
         model = User
         fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+
+        profile, created = Profile.objects.get_or_create(user=user)
+        profile.first_name = user.first_name
+        profile.last_name = user.last_name
+        profile.save()
+
+        return user
+
+
+class ProfileForm(forms.ModelForm):
+    
+    first_name = forms.CharField(max_length=30, required=True, label="First Name")
+    last_name = forms.CharField(max_length=30, required=True, label="Last Name")
+    image = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class': 'clearablefileinput'}), label='Profile picture')
+
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'last_name', 'tel', 'address',  'image']
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        
+        self.fields['first_name'].initial = self.instance.user.first_name
+        self.fields['last_name'].initial = self.instance.user.last_name
+
+    def save(self, commit=True):
+        
+        self.instance.user.first_name = self.cleaned_data['first_name']
+        self.instance.user.last_name = self.cleaned_data['last_name']
+
+        # Save the UserProfile instance
+        profile = super(ProfileForm, self).save(commit=commit)
+
+        if commit:
+            self.instance.user.save()
+
+        return profile
+
+
+class SetPasswordForm(SetPasswordForm):
+
+    class Meta:
+        model = get_user_model
+        fields = ['new_password1', 'new_password2']
+
+
+# class PasswordResetForm(PasswordResetForm):
+
+#     def __init__(self, *args, **kwargs):
+#         super(PasswordResetForm, self).__init__(*args, **kwargs)
